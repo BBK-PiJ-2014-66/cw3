f2aa1723a5370c7ef4588b8e0eda18c9d9ea0ff5
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-12-06 19:12:27 +0000

    start work on FunctionalArrayList, just stub ... for now.

---
+++ src/FunctionalArrayList.java
@@ -0,0 +1,40 @@
+/**
+ * FunctionalList implementation using ArrayList
+ * 
+ * A FunctionalList is a List with addition head and rest methods
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * 
+ */
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+	/**
+	 * Returns the element at the beginning of the list.
+	 * 
+	 * If the list is empty, an "appropriate error" is returned.
+	 * 
+	 * appropriate error means EMPTY_STRUCTURE
+	 * 
+	 * @return a copy of the element at the beginning of the list or an error if
+	 *         the list is empty.
+	 */
+	@Override
+	public ReturnObject head() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+    /**
+     * Returns a list with the elements in this list except the
+     * head. The elements must be in the same order. The original list
+     * must not change or be affected by changes in the new list. 
+     * 
+     * If the list is empty, another empty list is returned. 
+     */
+	@Override
+	public FunctionalList rest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
