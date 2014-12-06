15b9b916398646340c0fa5efa7b63ea7b924d94a
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-12-06 13:05:01 +0000

    Unit tests for LinkedList, all from ListTest initially get 7 errors and 2 failures with the "stub" version.

---
+++ src/LinkedListTest.java
@@ -0,0 +1,14 @@
+/**
+ * Unit test ArrayList
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+public class LinkedListTest extends ListTest {
+
+	/**
+	 * simply provide a List object using the ArrayList implementation
+	 */
+	public void initialize() {
+		myList = new LinkedList();
+	}
+	
+}
