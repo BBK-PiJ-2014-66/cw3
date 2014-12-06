5056127913468132a90649c9eeb69c202b90efb0
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-12-06 19:13:29 +0000

    JUnit tests to facilitate coding head method... in FunctionalArrayList. Following experience from ListTest develop using an abstract class to enable immediate reuse in FunctionalLinkedList.

---
+++ src/FunctionalArrayListTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test FunctionalArrayList class. 
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+public class FunctionalArrayListTest extends FunctionalListTest {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		myList = new FunctionalArrayList();
+	}
+
+}
---
+++ src/FunctionalListTest.java
@@ -0,0 +1,51 @@
+/**
+ * Unit test FunctionalList implementations.
+ *
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+public abstract class FunctionalListTest {
+	/**
+	 * start each test with an empty list
+	 */
+	FunctionalList myList;
+
+	@Before
+	public abstract void initialize();
+
+	@Test
+	public void testHeadonFilledList() {
+		myList.add("AA");
+		myList.add("BB");
+		myList.add("CC");
+		ReturnObject ro = myList.head();
+		// should not have given error
+		assertFalse(ro.hasError());
+		// head should give first element added "AA"
+		assertEquals(ro.getReturnValue(),"AA");
+	}
+
+	@Test
+	public void testHeadonEmptyList() {
+		// to exercise things add an element 
+		myList.add("AA");
+		// now remove it
+		myList.remove(0);
+		ReturnObject ro = myList.head();
+		// should have given error
+		assertTrue(ro.hasError());
+		// the error should be
+		assertEquals(ro.getError(),ErrorMessage.EMPTY_STRUCTURE);
+	}
+	
+	
+	@Test
+	public void testRest() {
+		fail("testRest not yet implemented"); // TODO
+	}
+
+}
