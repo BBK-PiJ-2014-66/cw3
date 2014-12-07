8513f1aa6ac7c763b70036913e8f0896e2312516
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-12-07 15:52:08 +0000

    describe test better

--- src/FunctionalArrayListTest.java
+++ src/FunctionalArrayListTest.java
@@ -1,13 +1,18 @@
 /**
- * Unit test FunctionalArrayList class. 
+ * Unit test FunctionalArrayList class.
  * 
  * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
  */
 public class FunctionalArrayListTest extends FunctionalListTest {
 
+	/**
+	 * initialize provides a new list of the correct class for the start of each
+	 * separate test.
+	 * 
+	 * the "meat" of the tests is in FunctionalListTest
+	 */
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
 		myList = new FunctionalArrayList();
 	}
 
