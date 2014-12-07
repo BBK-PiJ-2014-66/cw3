1025e2a7b7d538ed4f366ef32b63fb467a2d78f1
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-12-07 14:02:42 +0000

    Write and properly test head method.

--- src/FunctionalArrayList.java
+++ src/FunctionalArrayList.java
@@ -6,7 +6,8 @@
  * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
  * 
  */
-public class FunctionalArrayList extends ArrayList implements FunctionalList {
+public class FunctionalArrayList extends ArrayList implements FunctionalList,
+		Cloneable {
 
 	/**
 	 * Returns the element at the beginning of the list.
@@ -20,30 +21,50 @@ public class FunctionalArrayList extends ArrayList implements FunctionalList {
 	 */
 	@Override
 	public ReturnObject head() {
-		if (this.size()==0) {
+		if (this.size() == 0) {
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		}
 		return new ReturnObjectImpl(this.get(0).getReturnValue());
 	}
-	
-    /**
-     * Returns a list with the elements in this list except the
-     * head. The elements must be in the same order. The original list
-     * must not change or be affected by changes in the new list. 
-     * 
-     * If the list is empty, another empty list is returned. 
-     */
+
+	/**
+	 * Returns a list with the elements in this list except the head. The
+	 * elements must be in the same order. The original list must not change or
+	 * be affected by changes in the new list.
+	 * 
+	 * If the list is empty, another empty list is returned.
+	 */
 	@Override
 	public FunctionalList rest() {
-		// use a static method to return a new FunctionalList without the head 
-		FunctionalList chopOutHead = chopOutHead(this);
-		return chopOutHead;
+		// we have to "clone" the FunctionalList to get a new copy
+		FunctionalList cutDownList = this.clone();
+		// chop out the first (0 index) element if there is one
+		if (!cutDownList.isEmpty()) {
+			cutDownList.remove(0);
+		}
+		return cutDownList;
 	}
-	
-	public static FunctionalList chopOutHead( FunctionalList inFnList) {
-		// remove the first item ignoring any error
-		inFnList.remove(0);
-		return inFnList;
+
+	/**
+	 * clone of the current FunctionalList
+	 * 
+	 * see: https://docs.oracle.com/javase/7/docs/
+	 * api/java/lang/Object.html#clone%28%29
+	 * 
+	 * Notice clone is "shallow" as it includes the same objects not clones of
+	 * them.
+	 * 
+	 * @return the clone
+	 */
+	@Override
+	public FunctionalList clone() {
+		FunctionalList theClone = new FunctionalArrayList();
+		for (int ic = 0; ic < this.size(); ic++) {
+			Object item = this.get(ic).getReturnValue();
+			// assume we want a "shallow" copy so simply add the same object
+			theClone.add(item);
+		}
+		return theClone;
 	}
 
 }
--- src/FunctionalListTest.java
+++ src/FunctionalListTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
-
 public abstract class FunctionalListTest {
 	/**
 	 * start each test with an empty list
@@ -26,12 +25,12 @@ public abstract class FunctionalListTest {
 		// should not have given error
 		assertFalse(ro.hasError());
 		// head should give first element added "AA"
-		assertEquals("AA",ro.getReturnValue());
+		assertEquals("AA", ro.getReturnValue());
 	}
 
 	@Test
 	public void testHeadonEmptyList() {
-		// to exercise things add an element 
+		// to exercise things add an element
 		myList.add("AA");
 		// now remove it
 		myList.remove(0);
@@ -39,19 +38,42 @@ public abstract class FunctionalListTest {
 		// should have given error
 		assertTrue(ro.hasError());
 		// the error should be
-		assertEquals(ro.getError(),ErrorMessage.EMPTY_STRUCTURE);
+		assertEquals(ro.getError(), ErrorMessage.EMPTY_STRUCTURE);
 	}
-	
-	
+
 	@Test
-	public void testRest() {
+	public void testRestOnFilled() {
 		myList.add("AA");
 		myList.add("BB");
 		myList.add("CC");
-		assertEquals(3,myList.size());
+		assertEquals(3, myList.size());
 		FunctionalList chopList = myList.rest();
-		assertEquals(3,myList.size());
-		assertEquals(2,chopList.size());
+		// original list should still have 3 items
+		assertEquals(3, myList.size());
+		// chopList should have 2 items
+		assertEquals(2, chopList.size());
+		// remove the 2nd item "BB" in original list
+		myList.remove(1);
+		// chopList should have 2 items
+		assertEquals(2, chopList.size());
+		// and its head should be BB
+		assertEquals("BB", chopList.head().getReturnValue());
+		// chop off the next, leaving CC
+		chopList = chopList.rest();
+		assertEquals("CC", chopList.head().getReturnValue());
+		// and again to empty it
+		chopList = chopList.rest();
+		assertTrue(chopList.isEmpty());
+        // original list should have 2 items AA and CC		
+		assertEquals(2, myList.size());
+		assertEquals("AA", myList.head().getReturnValue());
+		assertEquals("CC", myList.get(1).getReturnValue());		
+	}
+	
+	@Test
+	public void testRestOnEmpty() {
+		FunctionalList chopList = myList.rest();
+		assertTrue(chopList.isEmpty());
 	}
 
 }
