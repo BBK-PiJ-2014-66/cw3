bee9aab3fcf30c6524ddc9c57ad9824c2028198e
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-01-05 17:15:10 +0000

    All tests now run and fail (as they should) for ImprovedStackImpl, tests pass for the two other implementations. Tests now have descriptions to allow easier tracing of failure.

--- src/StackImplTest.java
+++ src/StackImplTest.java
@@ -19,13 +19,18 @@ public class StackImplTest {
 		Stacks = new Stack[NSTACKS];
 		Stacks[0] = new StackImpl(new ArrayList());
 		Stacks[1] = new StackImpl(new LinkedList());
-		Stacks[2] = new ImprovedStackImpl();
+		if (NSTACKS > 2) {
+			Stacks[2] = new ImprovedStackImpl();
+		}
 	}
 
 	@Test
 	public void testEmptyStackisEmpty() {
+		String descript;
 		for (int sc = 0; sc < NSTACKS; sc++) {
-			assertTrue(Stacks[sc].isEmpty());
+			descript = "test for " + DESCRIBEIMPL[sc]
+					+ " : a empty list should  give .isEmpty true";
+			assertTrue(descript, Stacks[sc].isEmpty());
 		}
 	}
 
@@ -68,33 +73,72 @@ public class StackImplTest {
 
 	@Test
 	public void testTopFromFilled() {
+		String descript;
 		for (int sc = 0; sc < NSTACKS; sc++) {
 			Stacks[sc].push("AA");
 			Stacks[sc].push("B");
 			Stacks[sc].push("CCC");
 			ReturnObject ro = Stacks[sc].top();
-			assertFalse(ro.hasError());
-			// stack is last in first out so should get "CCC"
-			assertEquals("CCC", ro.getReturnValue());
+			if (ro == null) {
+				descript = "test A for " + DESCRIBEIMPL[sc]
+						+ " on filled stack .top should not return null object";
+				assertTrue(descript, false);
+			} else {
+				descript = "test B for "
+						+ DESCRIBEIMPL[sc]
+						+ " .top on filled stack should not produce .hasError().";
+				assertFalse(descript, ro.hasError());
+				// stack is last in first out so should get "CCC"
+				// make test more challenging by poping the last value
+				Stacks[sc].pop();
+				descript = "test C for "
+						+ DESCRIBEIMPL[sc]
+						+ " .top on filled stack should get back last object put in.";
+				assertEquals("CCC", ro.getReturnValue());
+			}
 		}
 	}
 
 	@Test
 	public void testPopFromFilled() {
+		String descript;
 		for (int sc = 0; sc < NSTACKS; sc++) {
 			Stacks[sc].push("A");
 			Stacks[sc].push("B");
 			ReturnObject ro = Stacks[sc].pop();
-			assertFalse(ro.hasError());
-			// stack is last in first out so should get "B"
-			assertEquals("B", ro.getReturnValue());
-			// stack should have one item
-			assertEquals(1, Stacks[sc].size());
-			assertEquals("A", Stacks[sc].pop().getReturnValue());
-			// another pop should give an EMPTRY_STRUCTURE error
-			ro = Stacks[sc].pop();
-			assertTrue(ro.hasError());
-			assertEquals(ErrorMessage.EMPTY_STRUCTURE, ro.getError());
+			if (ro == null) {
+				descript = "test A for " + DESCRIBEIMPL[sc]
+						+ " on filled stack .pop should not return null object";
+				assertTrue(descript, false);
+			} else {
+				descript = "test B for " + DESCRIBEIMPL[sc]
+						+ " on filled stack .pop should not return .hasError";
+				assertFalse(descript, ro.hasError());
+				// stack is last in first out so should get "B"
+				descript = "test C for "
+						+ DESCRIBEIMPL[sc]
+						+ " .pop on filled stack should get back last object put in.";
+				assertEquals(descript, "B", ro.getReturnValue());
+				// stack should have one item
+				descript = "test D for "
+						+ DESCRIBEIMPL[sc]
+						+ " .pop on filled stack with two items should give one item.";
+				assertEquals(descript, 1, Stacks[sc].size());
+				descript = "test E for "
+						+ DESCRIBEIMPL[sc]
+						+ " (2nd) .pop on filled stack should get back last object put in.";
+				assertEquals(descript,"A", Stacks[sc].pop().getReturnValue());
+				// another pop should give an EMPTY_STRUCTURE error
+				ro = Stacks[sc].pop();
+				descript = "test F for "
+						+ DESCRIBEIMPL[sc]
+						+ " .pop on now empty stack should give .hasError";
+				assertTrue(descript,ro.hasError());
+				descript = "test G for "
+						+ DESCRIBEIMPL[sc]
+						+ " .pop on now empty stack should give ErrorMessage EMPTY_STRUCTURE";
+				assertEquals(descript,ErrorMessage.EMPTY_STRUCTURE, ro.getError());
+			}
 		}
 	}
 
