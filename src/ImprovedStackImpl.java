dc4f5c5fd91d2c3a12c3eeeb80cea15b66d1da7c
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-01-05 16:12:03 +0000

    "Stub" implementation of ImprovedStackImpl does not actually do anything yet but enables JUnit tests to be developed.

---
+++ src/ImprovedStackImpl.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ *
+ */
+public class ImprovedStackImpl implements ImprovedStack {
+
+	//private Stack internalStack;
+		
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void push(Object item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ReturnObject top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReturnObject pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImprovedStack reverse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Object object) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
