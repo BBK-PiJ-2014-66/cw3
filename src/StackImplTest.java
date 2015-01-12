361282d53f609f597a36aae0ac73b29d4e3f56f0
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2015-01-12 11:53:01 +0000

    use Assert.assertNotNull rather than testing for null produces cleaner code.
    Complete the JUnit documentation of each tests.
    
    All tests pass for the StackImpl with ArrayList and LinkedList
    but fail for the stub method version of ImprovedStackImpl.

--- src/StackImplTest.java
+++ src/StackImplTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +19,7 @@ import org.junit.Test;
  * and not the additional reverse and remove methods that the are introduced in
  * ImprovedStack
  * 
- * @author osmart
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
  * 
  */
 public class StackImplTest {
@@ -48,12 +49,13 @@ public class StackImplTest {
 	}
 
 	/**
-	 * initial stacks will be empty. Check that they actually give .isEmpty() true
+	 * initial stacks will be empty. Check that they actually give .isEmpty()
+	 * true
 	 */
 	@Test
 	public void testEmptyStackisEmpty() {
 		// in the case of a failure need to know which fails
-		String descript; 
+		String descript;
 		for (int sc = 0; sc < NSTACKS; sc++) {
 			descript = "test for " + DESCRIBEIMPL[sc]
 					+ " : a empty list should  give .isEmpty true";
@@ -62,8 +64,8 @@ public class StackImplTest {
 	}
 
 	/**
-	 * test that three things to a stack results in .isEmpty false and
-	 * .size() of three.
+	 * test that three things to a stack results in .isEmpty false and .size()
+	 * of three.
 	 */
 	@Test
 	public void testStackSize() {
@@ -82,29 +84,31 @@ public class StackImplTest {
 	}
 
 	/**
-	 * test top from empty stack. Should produce a EMPTY_STRUCTURE error message
+	 * test top from empty stack. Should produce a EMPTY_STRUCTURE error
+	 * message.
 	 */
 	@Test
 	public void testTopFromEmpty() {
 		String descript;
 		for (int sc = 0; sc < NSTACKS; sc++) {
 			ReturnObject ro = Stacks[sc].top();
-			if (ro == null) {
-				descript = "test A for " + DESCRIBEIMPL[sc]
-						+ " .top should not return null object";
-				assertTrue(descript, false);
-			} else {
-				descript = "test B for " + DESCRIBEIMPL[sc]
-						+ " .top on empty list should produce .hasError().";
-				assertTrue(descript, ro.hasError());
-				descript = "test C for "
-						+ DESCRIBEIMPL[sc]
-						+ " .top on empty list should produce EMPTY_STRUCTURE error message";
-				assertEquals(ErrorMessage.EMPTY_STRUCTURE, ro.getError());
-			}
+			descript = "test A for " + DESCRIBEIMPL[sc]
+					+ " .top should never return a null!";
+			assertNotNull(descript, ro);
+			descript = "test B for " + DESCRIBEIMPL[sc]
+					+ " .top on empty list should produce .hasError().";
+			assertTrue(descript, ro.hasError());
+			descript = "test C for "
+					+ DESCRIBEIMPL[sc]
+					+ " .top on empty list should produce EMPTY_STRUCTURE error message";
+			assertEquals(ErrorMessage.EMPTY_STRUCTURE, ro.getError());
 		}
 	}
 
+	/**
+	 * push three objects onto the stack and test that top gets back the last
+	 * one put in
+	 */
 	@Test
 	public void testTopFromFilled() {
 		String descript;
@@ -113,26 +117,27 @@ public class StackImplTest {
 			Stacks[sc].push("B");
 			Stacks[sc].push("CCC");
 			ReturnObject ro = Stacks[sc].top();
-			if (ro == null) {
-				descript = "test A for " + DESCRIBEIMPL[sc]
-						+ " on filled stack .top should not return null object";
-				assertTrue(descript, false);
-			} else {
-				descript = "test B for "
-						+ DESCRIBEIMPL[sc]
-						+ " .top on filled stack should not produce .hasError().";
-				assertFalse(descript, ro.hasError());
-				// stack is last in first out so should get "CCC"
-				// make test more challenging by poping the last value
-				Stacks[sc].pop();
-				descript = "test C for "
-						+ DESCRIBEIMPL[sc]
-						+ " .top on filled stack should get back last object put in.";
-				assertEquals("CCC", ro.getReturnValue());
-			}
+			descript = "test A for " + DESCRIBEIMPL[sc]
+					+ " on filled stack .top should not return null!";
+			assertNotNull(descript, ro);
+			descript = "test B for " + DESCRIBEIMPL[sc]
+					+ " .top on filled stack should not produce .hasError().";
+			assertFalse(descript, ro.hasError());
+			// stack is last in first out so should get "CCC"
+			// make test more challenging by poping the last value
+			Stacks[sc].pop();
+			descript = "test C for "
+					+ DESCRIBEIMPL[sc]
+					+ " .top on filled stack should get back last object put in.";
+			assertEquals("CCC", ro.getReturnValue());
 		}
 	}
 
+	/**
+	 * push two objects onto the stack. Pop one out. Check that correct object
+	 * is returned. Pop all objects out. Do an additional pop and check that
+	 * EMPTY_STRUCTURE error message is found.
+	 */
 	@Test
 	public void testPopFromFilled() {
 		String descript;
@@ -140,39 +145,35 @@ public class StackImplTest {
 			Stacks[sc].push("A");
 			Stacks[sc].push("B");
 			ReturnObject ro = Stacks[sc].pop();
-			if (ro == null) {
-				descript = "test A for " + DESCRIBEIMPL[sc]
-						+ " on filled stack .pop should not return null object";
-				assertTrue(descript, false);
-			} else {
-				descript = "test B for " + DESCRIBEIMPL[sc]
-						+ " on filled stack .pop should not return .hasError";
-				assertFalse(descript, ro.hasError());
-				// stack is last in first out so should get "B"
-				descript = "test C for "
-						+ DESCRIBEIMPL[sc]
-						+ " .pop on filled stack should get back last object put in.";
-				assertEquals(descript, "B", ro.getReturnValue());
-				// stack should have one item
-				descript = "test D for "
-						+ DESCRIBEIMPL[sc]
-						+ " .pop on filled stack with two items should give one item.";
-				assertEquals(descript, 1, Stacks[sc].size());
-				descript = "test E for "
-						+ DESCRIBEIMPL[sc]
-						+ " (2nd) .pop on filled stack should get back last object put in.";
-				assertEquals(descript, "A", Stacks[sc].pop().getReturnValue());
-				// another pop should give an EMPTY_STRUCTURE error
-				ro = Stacks[sc].pop();
-				descript = "test F for " + DESCRIBEIMPL[sc]
-						+ " .pop on now empty stack should give .hasError";
-				assertTrue(descript, ro.hasError());
-				descript = "test G for "
-						+ DESCRIBEIMPL[sc]
-						+ " .pop on now empty stack should give ErrorMessage EMPTY_STRUCTURE";
-				assertEquals(descript, ErrorMessage.EMPTY_STRUCTURE,
-						ro.getError());
-			}
+			descript = "test A for " + DESCRIBEIMPL[sc]
+					+ " on filled stack .pop should not return null object";
+			assertNotNull(descript, ro);
+			descript = "test B for " + DESCRIBEIMPL[sc]
+					+ " on filled stack .pop should not return .hasError";
+			assertFalse(descript, ro.hasError());
+			// stack is last in first out so should get "B"
+			descript = "test C for "
+					+ DESCRIBEIMPL[sc]
+					+ " .pop on filled stack should get back last object put in.";
+			assertEquals(descript, "B", ro.getReturnValue());
+			// stack should have one item
+			descript = "test D for "
+					+ DESCRIBEIMPL[sc]
+					+ " .pop on filled stack with two items should give one item.";
+			assertEquals(descript, 1, Stacks[sc].size());
+			descript = "test E for "
+					+ DESCRIBEIMPL[sc]
+					+ " (2nd) .pop on filled stack should get back last object put in.";
+			assertEquals(descript, "A", Stacks[sc].pop().getReturnValue());
+			// another pop should give an EMPTY_STRUCTURE error
+			ro = Stacks[sc].pop();
+			descript = "test F for " + DESCRIBEIMPL[sc]
+					+ " .pop on now empty stack should give .hasError";
+			assertTrue(descript, ro.hasError());
+			descript = "test G for "
+					+ DESCRIBEIMPL[sc]
+					+ " .pop on now empty stack should give ErrorMessage EMPTY_STRUCTURE";
+			assertEquals(descript, ErrorMessage.EMPTY_STRUCTURE, ro.getError());
 		}
 	}
 
